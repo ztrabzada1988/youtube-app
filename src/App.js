5d65f2d37358daa6de15112c468560971516bb44
Zubair Trabzada <zubair.trabzada@gmail.com>
2018-11-19 11:34:57 -0800

    adding background image and deploying to gh-pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       videos: [],
       selectedVideo: null,
     };
-    this.videoSearch('dubai');
+    this.videoSearch('the palm island');
   }
 
   videoSearch(term) {
@@ -30,7 +30,6 @@ class App extends Component {
     });  
   }
   
-
    render() {
     const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
 
--- src/youtube_api.js
+++ src/youtube_api.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
 
+const API_Key = 'AIzaSyDxFT6JiS4QdtSoxbDcj7gklbTA1_7J54I'; 
 
 export default API_Key;
